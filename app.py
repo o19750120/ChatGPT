24bc22ddae5e9bae3306b82e7db58e23be64e965
Oliver Liu <75584574+o19750120@users.noreply.github.com>
2023-10-29 23:07:25 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask
+import os
+
 app = Flask(__name__)
+
+openai.api_key = os.environ.get("OPENAI_API_KEY")
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'Hello, World!', openai.api_key
