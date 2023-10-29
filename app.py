f2bcc6bdafe6b8ca95ce41c6400b96fa5c9aaef2
Oliver Liu <75584574+o19750120@users.noreply.github.com>
2023-10-29 16:44:01 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,39 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+import openai
+from dotenv import load_dotenv
+import os
+
+# 載入環境變量
+load_dotenv(".env")
+openai.api_key = os.getenv("api_key")
+
+app = FastAPI()
+
+class TextInput(BaseModel):
+    text: str
+
+def generate_summary(text: str) -> dict:
+    prompt = """
+        根據以下描述的內容，擷取前五個字。
+
+        {text}
+    """.format(text=text)
+
+    res = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=prompt,
+        max_tokens=100
+    )
+
+    summary = res["choices"][0]["text"].strip()
+
+    return {"summary": summary}
+
+@app.post("/generate-summary")
+def generate_summary_endpoint(item: TextInput):
+    summary = generate_summary(item.text)
+    return summary
+
+# 啟動指令如果你使用 uvicorn 來啟動 FastAPI
+# uvicorn filename:app --reload
