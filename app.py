7288851abef8398c2c28742371a4530ba1d3cc84
Oliver Liu <75584574+o19750120@users.noreply.github.com>
2023-10-29 23:01:57 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,36 +1,6 @@
-import os
-from flask import Flask, request, jsonify
-import openai
-import json
-
-openai.api_key = os.environ.get("OPENAI_API_KEY")
-
+from flask import Flask
 app = Flask(__name__)
 
-def generate_summary(text):
-    prompt = """
-        根據以下描述的內容，擷取前五個字。
-
-        {text}
-    """.format(text=text)
-
-    res = openai.Completion.create(
-        model = "text-davinci-003",
-        prompt = prompt,
-        max_tokens = 100
-    )
-
-    summary = res["choices"][0]["text"].strip()
-
-    return {"summary": summary}
-
-
-@app.route('/generate-summary', methods=['POST'])
-def generate_summary_endpoint():
-    data = request.get_json()
-    text = data["text"]
-    summary = generate_summary(text)
-    return jsonify(summary)
-
-if __name__ == "__main__":
-    app.run(port=5000, debug=True)
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
